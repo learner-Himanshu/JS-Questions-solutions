6705d081d7d0927b6275379e66ce3a8f8210848d
Himanshu <144791918+learner-Himanshu@users.noreply.github.com>
2024-02-02 08:50:08 +0000

    sum of 1 to 10 factorial of 10! and

---
+++ Questions/calculate1To10.js
@@ -0,0 +1,33 @@
+// let sum=0;
+// for(let i=0; i<=10; i++){
+//     sum = sum + i;
+// }
+// console.log(sum)
+
+
+
+// //                        ===> factorial of 10
+
+
+// let factorial=1;
+// for(let i =10; i>=1; i--){
+//     console.log(`i is; ${i} and fatorial is ${factorial}`)
+//     factorial = factorial*i
+//     console.log(`i is; ${i} and fatorial is ${factorial}`)
+// }
+// console.log(`Factorial of 10 is ${factorial}`)
+
+
+// claculating the sum of even numbers between 10 to 30;
+
+
+
+let result=0;
+for(let i=11; i<30; i++){
+    if(i%2==0){
+        console.log(i)
+        result = result + i; 
+    }
+}
+console.log(`sum of all even numbers between 10 to 30 is : ${result}`)
+
