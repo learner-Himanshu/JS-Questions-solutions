2c243eeed23d494a166d63a2775177c24f09d8a8
Himanshu <144791918+learner-Himanshu@users.noreply.github.com>
2024-02-02 06:59:00 +0000

    used for,while and doWhile Loop

---
+++ Questions/OneToTen.js
@@ -0,0 +1,25 @@
+                    // =====>>>       using for loop
+
+// for(let i=1; i<=10; i++){
+//       console.log(i)
+// }
+
+            // =========>     using while loop
+
+
+// let i=1;
+// while(i<=10){
+//     console.log(i)
+//     i++
+// }
+
+let i = 1;
+
+do{
+    console.log(i)
+    i++
+    // console.log(i)
+}while(i<=10);
+
+
+
