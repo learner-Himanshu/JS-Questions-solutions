96b52b426176fcf158a7306674f7b46d5a2b794c
Himanshu <144791918+learner-Himanshu@users.noreply.github.com>
2024-02-02 07:45:04 +0000

    Even and Odd number code

---
+++ Questions/oddNumbers.js
@@ -0,0 +1,16 @@
+// for(let i=0; i<=100; i++){
+//     if(i%2){
+        
+//         console.log("it's Odd number: ",i)
+//     }
+   
+// }
+
+        //   ==========>>>>   even numbers
+
+        for(let j =0; j<=100; j++){
+            if(j%2==0){
+                console.log(j)
+            }
+        }
+
