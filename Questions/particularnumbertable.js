040413dd47b87b0343907d3b98eea9d36b0f955a
Himanshu <144791918+learner-Himanshu@users.noreply.github.com>
2024-02-02 07:53:21 +0000

    partucular number table and multiple number's table in once

---
+++ Questions/particularnumbertable.js
@@ -0,0 +1,13 @@
+for(let i=1; i<=10; i++){
+    console.log(`7 * ${i} = `, 7*i)
+}
+
+
+// printing table of 1 to 10 in once
+
+
+for(let i=1; i<=10; i++){
+    for(let j=1; j<=10; j++){
+        console.log(`${i} * ${j} = `,i*j)
+    }
+}
