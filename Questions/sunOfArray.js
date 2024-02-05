d007e00d9deae39b0cb85202984c91c5c53f17ac
Himanshu <144791918+learner-Himanshu@users.noreply.github.com>
2024-02-05 07:31:21 +0000

    average of an array element

--- Questions/sunOfArray.js
+++ Questions/sunOfArray.js
@@ -11,3 +11,18 @@ let arr = [1,2,3,4,5,6,7]
 let result = addFunction(arr)
 console.log(result)
 
+
+// ===========> average of  array element
+
+
+function Average(arr){
+    let total = 0;
+    let length= arr.length;
+ arr.forEach((value)=>{
+    total = total+value;
+ })
+ return total/length;
+ 
+}
+  let avg = Average([1,2,3,4,5,6,7,8])
+  console.log(avg)
