f09e1eb180445b2671a1e01c5121a75dbccd8f28
Himanshu <144791918+learner-Himanshu@users.noreply.github.com>
2024-02-05 08:19:47 +0000

    print positive values of an array without  using inbuilt function

--- Questions/findingPositiveNumbers.js
+++ Questions/findingPositiveNumbers.js
@@ -1,5 +1,21 @@
-function positiveValues(array){
-    return array.filter(val => val>=0)
+// function positiveValues(array){
+//     return array.filter(val => val>=0)
+// }
+// let result = positiveValues([-1,2,3,-4,-5,5,6,-6])
+// console.log(result);
+
+// ==============>
+
+
+function posiVal(arr){
+    let positiveArray=[];
+for(let val =0; val<arr.length; val++){
+    if(arr[val]>=0){
+        positiveArray.push(arr[val])
+    }
 }
-let result = positiveValues([-1,2,3,-4,-5,5,6,-6])
-console.log(result);
+return positiveArray;
+}
+
+console.log(posiVal([-1,1,0,-3,-5,3,5,-9,8])) 
+
