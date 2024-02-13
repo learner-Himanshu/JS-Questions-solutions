9c2e76c6721ea666da6ef501e751d052b270a46d
Himanshu <144791918+learner-Himanshu@users.noreply.github.com>
2024-02-13 08:11:39 +0000

    Printing less value

--- Questions/fibonacci.js
+++ Questions/fibonacci.js
@@ -12,4 +12,34 @@ function fibonacci(n){
         console.log(fibonacciNumbers[j]);
         }
 }
-fibonacci(10);
+// fibonacci(10);
+
+
+// printing all the fibonacci numbers less then 100
+
+
+ function fibo(limit){
+    let fv= 0;
+    let sv=1;
+    let nv=fv+sv;
+    if(limit===0){
+        return 0;
+    }else {console.log(fv);}
+
+    if(limit===1){return 1} else{
+        console.log(sv);
+    }
+  
+    
+    for(let i=2; i<=limit; i++){
+        if(nv<limit){
+            console.log(nv)
+        }
+          fv=sv;
+          sv=nv;
+          nv=fv+sv;
+    }
+
+ }
+
+ fibo(50)
